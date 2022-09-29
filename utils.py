1d12a674d2e35d30ecef6ae14ff714869bd9a23f
SnowCharm <619022098@qq.com>
2022-09-29 11:49:12 +0800

    add exceptions.py and utils.py

---
+++ utils.py
@@ -0,0 +1,32 @@
+import os
+import numpy as np
+from PIL import Image
+
+
+def generate_data_path(path: str):
+    tif_path, volume_path = None, None
+    for filename in os.listdir(path):
+        suffix = os.path.splitext(filename)[-1]
+        if suffix == '.tif':
+            tif_path = os.path.join(path, filename)
+        elif suffix == '.fda_OCT_pngs':
+            images_path = os.path.join(path, filename)
+            volume = np.zeros((128, 885, 512), dtype=np.int64)
+            for i in range(128):
+                oct_path = f"octscan_{i + 1}.png"
+                oct_path = os.path.join(images_path, oct_path)
+                img = Image.open(oct_path)
+                img = np.array(img)
+                volume[i, :, :] = img
+            volume_path = os.path.join(path, "volume.npy")
+            np.save(volume_path, volume)
+    assert tif_path is not None
+    assert volume_path is not None
+    return tif_path, volume_path
+
+
+def clean_data_npy(path: str):
+    for filename in os.listdir(path):
+        if filename == 'volume.npy':
+            filename = os.path.join(path, filename)
+            os.remove(filename)
