ef944486ef0597c27161761938cdceb85ee13431
SnowCharm <619022098@qq.com>
2022-12-30 12:24:26 +0800

    add train.sh

---
+++ train.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -o job.%j.out
+#SBATCH --partition=titan
+#SBATCH --qos=titan
+#SBATCH -J trainJob
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=6
+#SBATCH --gres=gpu:1
+
+python -u train.py
