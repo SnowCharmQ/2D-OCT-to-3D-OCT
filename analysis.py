33d8a55902d451ba7f7139a38c66688661d56078
SnowCharm <619022098@qq.com>
2023-01-03 19:47:27 +0800

    add analysis.py to analyze the output files

---
+++ analysis.py
@@ -0,0 +1,107 @@
+import os
+import matplotlib.pyplot as plt
+
+
+def get_val(line: str, idx: int):
+    i = idx
+    while True:
+        if i == len(line) or line[i] == ' ':
+            break
+        else:
+            i += 1
+    return line[idx:i]
+
+
+def get_error_metrics(line: str):
+    line = line.replace("\n", "").replace("\t", "")
+    idx = line.index("mse: ") + 5
+    mse = float(get_val(line, idx))
+    idx = line.index("mae: ") + 5
+    mae = float(get_val(line, idx))
+    idx = line.index("rmse: ") + 6
+    rmse = float(get_val(line, idx))
+    idx = line.index("psnr: ") + 6
+    psnr = float(get_val(line, idx))
+    idx = line.index("ssim: ") + 6
+    ssim = float(get_val(line, idx))
+    return mse, mae, rmse, psnr, ssim
+
+
+for file_name in os.listdir('out'):
+    if os.path.isdir('out/' + file_name):
+        continue
+    f = open('out/' + file_name, 'r')
+    lines = f.readlines()
+    epoch = 0
+    x_axis = []
+    train_loss = []
+    mses, maes, rmses, psnrs, ssims = [], [], [], [], []
+    for line in lines:
+        if line.startswith("Start training in epoch"):
+            epoch += 1
+            x_axis.append(epoch)
+        if line.startswith("Finish Epoch: "):
+            index = line.index("Loss:")
+            index += 6
+            train_loss.append(float(line[index:].replace("\t", "")))
+        if line.startswith("Average"):
+            mse, mae, rmse, psnr, ssim = get_error_metrics(line)
+            mses.append(mse)
+            maes.append(mae)
+            rmses.append(rmse)
+            psnrs.append(psnr)
+            ssims.append(ssim)
+        if line.startswith("Start testing"):
+            break
+
+    dir_name = 'out/' + file_name.split(".")[0]
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+
+    plt.figure()
+    plt.plot(x_axis, train_loss, '-', color='#4169E1', alpha=0.8, linewidth=1, label="Train Loss")
+    plt.legend(loc="upper right")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title("Train Loss Chart")
+    plt.savefig(dir_name + "/loss.png")
+
+    plt.figure()
+    plt.plot(x_axis, mses, '-', color='#4169E1', alpha=0.8, linewidth=1, label="MSE")
+    plt.legend(loc="upper right")
+    plt.xlabel("Epoch")
+    plt.ylabel("MSE")
+    plt.title("Validation MSE Chart")
+    plt.savefig(dir_name + "/mse.png")
+
+    plt.figure()
+    plt.plot(x_axis, maes, '-', color='#4169E1', alpha=0.8, linewidth=1, label="MAE")
+    plt.legend(loc="upper right")
+    plt.xlabel("Epoch")
+    plt.ylabel("MAE")
+    plt.title("Validation MAE Chart")
+    plt.savefig(dir_name + "/mae.png")
+
+    plt.figure()
+    plt.plot(x_axis, rmses, '-', color='#4169E1', alpha=0.8, linewidth=1, label="RMSE")
+    plt.legend(loc="upper right")
+    plt.xlabel("Epoch")
+    plt.ylabel("RMSE")
+    plt.title("Validation RMSE Chart")
+    plt.savefig(dir_name + "/rmse.png")
+
+    plt.figure()
+    plt.plot(x_axis, psnrs, '-', color='#4169E1', alpha=0.8, linewidth=1, label="PSNR")
+    plt.legend(loc="lower right")
+    plt.xlabel("Epoch")
+    plt.ylabel("PSNR")
+    plt.title("Validation PSNR Chart")
+    plt.savefig(dir_name + "/psnr.png")
+
+    plt.figure()
+    plt.plot(x_axis, ssims, '-', color='#4169E1', alpha=0.8, linewidth=1, label="SSIM")
+    plt.legend(loc="lower right")
+    plt.xlabel("Epoch")
+    plt.ylabel("SSIM")
+    plt.title("Validation SSIM Chart")
+    plt.savefig(dir_name + "/ssim.png")
